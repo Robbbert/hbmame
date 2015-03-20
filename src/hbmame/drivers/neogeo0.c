78d62d6cce3871b05d884e2539df3b41615f7307
Robbbert <pac0446@bigpond.net.au>
2015-03-21 01:52:20 +1100

    Fix name conflict between sfix.sfix and sfix.sfx

--- src/hbmame/drivers/neogeo0.c
+++ src/hbmame/drivers/neogeo0.c
@@ -8,13 +8,13 @@
 #define NEO_SFIX_MT_128K \
 	ROM_Y_ZOOM \
 	ROM_REGION( 0x20000, "fixedbios", 0 ) \
-	ROM_LOAD( "sfix.sfx",  0, 0x20000, CRC(c2ea0cfd) SHA1(fd4a618cdcdbf849374f0a50dd8efe9dbab706c3) ) \
+	ROM_LOAD( "sfix.sfix",  0, 0x20000, CRC(c2ea0cfd) SHA1(fd4a618cdcdbf849374f0a50dd8efe9dbab706c3) ) \
 	ROM_REGION( 0x20000, "fixed", ROMREGION_ERASE00 )
 
 #define NEO_SFIX_MT_512K \
 	ROM_Y_ZOOM \
 	ROM_REGION( 0x20000, "fixedbios", 0 ) \
-	ROM_LOAD( "sfix.sfx",  0, 0x20000, CRC(c2ea0cfd) SHA1(fd4a618cdcdbf849374f0a50dd8efe9dbab706c3) ) \
+	ROM_LOAD( "sfix.sfix",  0, 0x20000, CRC(c2ea0cfd) SHA1(fd4a618cdcdbf849374f0a50dd8efe9dbab706c3) ) \
 	ROM_REGION( 0x80000, "fixed", ROMREGION_ERASE00 )
 
 #define NEO_SFIX_256K(name, hash) \
