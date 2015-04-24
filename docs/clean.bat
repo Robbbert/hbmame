b2c061444fb154748abfc1cb2a792b3eda3c1d1d
Robbbert <pac0446@bigpond.net.au>
2015-04-24 16:14:59 +1000

    Update of compile docs

---
+++ docs/clean.bat
@@ -0,0 +1,12 @@
+@echo off
+if %1q==q rd /s /q build
+if %1q==q del 3rdparty\genie\bin\windows\genie.exe
+if %1q==q goto end
+
+if exist ini\%1.ini del ini\%1.ini
+if exist cfg\%1.cfg del cfg\%1.cfg
+if exist nvram\%1.nv del nvram\%1.nv
+if exist nvram\%1 rd /s /q nvram\%1
+:end
+echo %1 cleaned
+
