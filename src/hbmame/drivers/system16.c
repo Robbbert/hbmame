89a9c8b1969564faa11798555afa6ebcaf849588
Robbbert <pac0446@bigpond.net.au>
2015-08-29 23:34:24 +1000

    Added back mslug3kho. Patched sys16dem to get it working.

--- src/hbmame/drivers/system16.c
+++ src/hbmame/drivers/system16.c
@@ -2,13 +2,15 @@
 // copyright-holders:Robbbert
 #include "../mame/drivers/system16.c"
 
-
-// HBMAME - freezes when it wants to do sprite processing
+// Sprite processing has been disabled because the sprite processor (sega16sp.c) freezes
+//  the moment anything is written to sprite ram.
 
 ROM_START( sys16dem )
 	ROM_REGION( 0x040000, "maincpu", ROMREGION_ERASEFF ) /* 68000 code */
 	ROM_LOAD16_BYTE( "rom2dm.bin", 0x00000, 0x10000, CRC(671c5eac) SHA1(5fa7f8ba9d31a6c31a0b5a32c2cd869200032461) )
 	ROM_LOAD16_BYTE( "rom1dm.bin", 0x00001, 0x10000, CRC(6461ebd9) SHA1(16dc373408342560eb48e3c037a8f42636796560) )
+	ROM_FILL(0x686, 1, 0x4e)
+	ROM_FILL(0x687, 1, 0x71) // cancel writing to sprite ram
 
 	ROM_REGION( 0x30000, "gfx1", 0 ) /* tiles */
 	ROM_LOAD( "scr01dm.b9",  0x00000, 0x10000, CRC(2fdb9b09) SHA1(bec9ca842921695b06915825acfba076443a7e8b) )
@@ -23,4 +25,4 @@ ROM_START( sys16dem )
 	ROM_LOAD( "epr12168.a7", 0x0000, 0x8000, CRC(bd9ba01b) SHA1(fafa7dc36cc057a50ae4cdf7a35f3594292336f4) )
 ROM_END
 
-GAME( 1988, sys16dem, 0, tetrisbl, tetris, segas1x_bootleg_state, dduxbl, ROT0, "Charles Doty", "Demo - Sega System 16", MACHINE_NOT_WORKING )
+GAME( 1988, sys16dem, 0, tetrisbl, tetris, segas1x_bootleg_state, dduxbl, ROT0, "Charles Doty", "Demo - Sega System 16", MACHINE_IMPERFECT_GRAPHICS )
--- src/hbmame/drivers/taito_f2.c
+++ src/hbmame/drivers/taito_f2.c
@@ -2,6 +2,64 @@
 // copyright-holders:Robbbert
 #include "../mame/drivers/taito_f2.c"
 
+static MACHINE_CONFIG_START( f2demo, taitof2_state )
+
+	/* basic machine hardware */
+	MCFG_CPU_ADD("maincpu", M68000, 24000000/2) /* 12 MHz */
+	MCFG_CPU_PROGRAM_MAP(liquidk_map)
+	MCFG_CPU_VBLANK_INT_DRIVER("screen", taitof2_state,  taitof2_interrupt)
+
+	MCFG_CPU_ADD("audiocpu", Z80, 24000000/6)   /* 4 MHz */
+	MCFG_CPU_PROGRAM_MAP(sound_map)
+
+	MCFG_MACHINE_START_OVERRIDE(taitof2_state,f2)
+
+	/* video hardware */
+	MCFG_SCREEN_ADD("screen", RASTER)
+	MCFG_SCREEN_REFRESH_RATE(60)
+	MCFG_SCREEN_VBLANK_TIME(ATTOSECONDS_IN_USEC(0)  /* frames per second, vblank duration */)
+	MCFG_SCREEN_SIZE(120*8, 32*8)
+	MCFG_SCREEN_VISIBLE_AREA(40*8, 106*8-1, 2*8, 32*8-1)
+	MCFG_SCREEN_UPDATE_DRIVER(taitof2_state, screen_update_taitof2_pri)
+	MCFG_SCREEN_VBLANK_DRIVER(taitof2_state, screen_eof_taitof2_partial_buffer_delayed)
+	MCFG_SCREEN_PALETTE("palette")
+	MCFG_GFXDECODE_ADD("gfxdecode", "palette", taitof2)
+	MCFG_PALETTE_ADD("palette", 4096)
+	MCFG_PALETTE_FORMAT(RRRRGGGGBBBBxxxx)
+	MCFG_VIDEO_START_OVERRIDE(taitof2_state,taitof2_megab)
+
+	/* sound hardware */
+	MCFG_SPEAKER_STANDARD_STEREO("lspeaker", "rspeaker")
+
+	MCFG_SOUND_ADD("ymsnd", YM2610, 24000000/3) /* Was 16000000/2, but only a 24Mhz OSC */
+	MCFG_YM2610_IRQ_HANDLER(WRITELINE(taitof2_state, irqhandler))
+	MCFG_SOUND_ROUTE(0, "lspeaker",  0.25)
+	MCFG_SOUND_ROUTE(0, "rspeaker", 0.25)
+	MCFG_SOUND_ROUTE(1, "lspeaker",  1.0)
+	MCFG_SOUND_ROUTE(2, "rspeaker", 1.0)
+
+	MCFG_DEVICE_ADD("tc0140syt", TC0140SYT, 0)
+	MCFG_TC0140SYT_MASTER_CPU("maincpu")
+	MCFG_TC0140SYT_SLAVE_CPU("audiocpu")
+
+	MCFG_DEVICE_ADD("tc0220ioc", TC0220IOC, 0)
+	MCFG_TC0220IOC_READ_0_CB(IOPORT("DSWA"))
+	MCFG_TC0220IOC_READ_1_CB(IOPORT("DSWB"))
+	MCFG_TC0220IOC_READ_2_CB(IOPORT("IN0"))
+	MCFG_TC0220IOC_READ_3_CB(IOPORT("IN1"))
+	MCFG_TC0220IOC_READ_7_CB(IOPORT("IN2"))
+
+	MCFG_DEVICE_ADD("tc0100scn", TC0100SCN, 0)
+	MCFG_TC0100SCN_GFX_REGION(1)
+	MCFG_TC0100SCN_TX_REGION(2)
+	MCFG_TC0100SCN_OFFSETS(3, 0)
+	MCFG_TC0100SCN_GFXDECODE("gfxdecode")
+	MCFG_TC0100SCN_PALETTE("palette")
+
+	MCFG_TC0360PRI_ADD("tc0360pri")
+MACHINE_CONFIG_END
+
+// Game has no sound
 ROM_START( f2demo )
 	ROM_REGION( 0x80000, "maincpu", 0 )     /* 512k for 68000 code */
 	ROM_LOAD16_BYTE( "lq09.bin",  0x00000, 0x20000, CRC(809a968b) SHA1(06e8be120869371d33c61fa14c48fe4e5fd659ec) )
@@ -24,4 +82,4 @@ ROM_START( f2demo )
 	/* no Delta-T samples */
 ROM_END
 
-GAME( 1990, f2demo, 0, liquidk, liquidk, driver_device, 0, ROT180, "Charles Doty", "Demo - Taito F2", MACHINE_SUPPORTS_SAVE )
+GAME( 1990, f2demo, 0, f2demo, liquidk, driver_device, 0, ROT180, "Charles Doty", "Demo - Taito F2", MACHINE_SUPPORTS_SAVE )
