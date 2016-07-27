437eeecc020c81384491dc193bf85a8ed5994b56
Robbbert <Robbbert@users.noreply.github.com>
2016-07-27 17:43:26 +1000

    Version up

--- src/version.cpp
+++ src/version.cpp
@@ -8,7 +8,7 @@
 
 ***************************************************************************/
 
-#define BARE_BUILD_VERSION "0.175"
+#define BARE_BUILD_VERSION "0.176"
 
 extern const char bare_build_version[];
 extern const char build_version[];
