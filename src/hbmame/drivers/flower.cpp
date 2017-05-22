d5e18a7b528f7fc756534172c00abae5836e99e4
Robbbert <Robbbert@users.noreply.github.com>
2017-05-22 12:29:29 +1000

    *Fixed the build.

--- src/hbmame/drivers/flower.cpp
+++ src/hbmame/drivers/flower.cpp
@@ -529,7 +529,7 @@ static MACHINE_CONFIG_START( flower )
 	MCFG_SCREEN_PALETTE("palette")
 
 	MCFG_GFXDECODE_ADD("gfxdecode", "palette", flower)
-	MCFG_PALETTE_ADD_RRRRGGGGBBBB_PROMS("palette", 256)
+	MCFG_PALETTE_ADD_RRRRGGGGBBBB_PROMS("palette", "proms", 256)
 
 	/* sound hardware */
 	MCFG_SPEAKER_STANDARD_MONO("mono")
