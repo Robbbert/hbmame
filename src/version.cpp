2ec0df858fc8f6ff7baeac3a43122f4fcca2e9c7
Robbbert <Robbbert@users.noreply.github.com>
2017-06-28 13:56:04 +1000

    *Fixed the build

--- src/version.cpp
+++ src/version.cpp
@@ -8,7 +8,7 @@
 
 ***************************************************************************/
 
-#define BARE_BUILD_VERSION "0.186"
+#define BARE_BUILD_VERSION "0.187"
 
 extern const char bare_build_version[];
 extern const char build_version[];
