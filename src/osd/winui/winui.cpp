e374d07b74b2a67c314f90174da2d313402cc279
Robbbert <Robbbert@users.noreply.github.com>
2017-07-22 00:04:00 +1000

    *Added new history.cpp

--- src/osd/winui/winui.cpp
+++ src/osd/winui/winui.cpp
@@ -2943,7 +2943,7 @@ static void UpdateHistory(void)
 
 	if (GetSelectedPick() >= 0)
 	{
-		char *histText = GetGameHistory(Picker_GetSelectedItem(hwndList));
+		char *histText = GetArcadeHistory(Picker_GetSelectedItem(hwndList));
 
 		have_history = (histText && histText[0]) ? TRUE : FALSE;
 		win_set_window_text_utf8(GetDlgItem(hMain, IDC_HISTORY), histText);
