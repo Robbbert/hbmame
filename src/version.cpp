426c9262479bc9f91ab4214c40f4b3299509093f
Robbbert <Robbbert@users.noreply.github.com>
2017-08-30 15:27:27 +1000

    *bump

--- src/version.cpp
+++ src/version.cpp
@@ -8,7 +8,7 @@
 
 ***************************************************************************/
 
-#define BARE_BUILD_VERSION "0.188"
+#define BARE_BUILD_VERSION "0.189"
 
 extern const char bare_build_version[];
 extern const char build_version[];
