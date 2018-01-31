6488544820bbf9bc96bd2f29bbc0a8b7c0b246fa
Robbbert <Robbbert@users.noreply.github.com>
2018-01-31 23:34:22 +1100

    *last updates

--- src/version.cpp
+++ src/version.cpp
@@ -8,7 +8,7 @@
 
 ***************************************************************************/
 
-#define BARE_BUILD_VERSION "0.191"
+#define BARE_BUILD_VERSION "0.193"
 
 extern const char bare_build_version[];
 extern const char build_version[];
