4c665a1bc762eefe6edd1e6eae59b356adff301f
Robbbert <Robbbert@users.noreply.github.com>
2018-03-03 00:49:55 +1100

    *more rom renaming

--- src/hbmame/drivers/igs011.cpp
+++ src/hbmame/drivers/igs011.cpp
@@ -29,13 +29,11 @@ ROM_START( lhb2s01 )
 	ROM_REGION( 0x200000, "blitter", 0 )
 	ROM_LOAD( "igsm0501.u7", 0x00000, 0x200000, CRC(1c952bd6) SHA1(a6b6f1cdfb29647e81c032ffe59c94f1a10ceaf8) )
 
-	ROM_REGION( 0x80000, "blitter_hi", 0 ) // high order bit of graphics (5th bit)
-	/* these are identical ..seems ok as igs number is same, only ic changed */
+	ROM_REGION( 0x80000, "blitter_hi", 0 )
 	ROM_LOAD( "igsm0502.u4", 0x00000, 0x80000, CRC(5d73ae99) SHA1(7283aa3d6b15ceb95db80756892be46eb997ef15) )
-	ROM_LOAD( "igsm0502.u5", 0x00000, 0x80000, CRC(5d73ae99) SHA1(7283aa3d6b15ceb95db80756892be46eb997ef15) )
 
 	ROM_REGION( 0x80000, "oki", 0 )
-	ROM_LOAD( "igss0503.u38", 0x00000, 0x80000, CRC(c9609c9c) SHA1(f036e682b792033409966e84292a69275eaa05e5) )  // 2 banks
+	ROM_LOAD( "igss0503.u38", 0x00000, 0x80000, CRC(c9609c9c) SHA1(f036e682b792033409966e84292a69275eaa05e5) )
 ROM_END
 
 /*************************************
