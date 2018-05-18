449a0e2118bc50d5f85a6a6d08be5c52f425edda
Robbbert <Robbbert@users.noreply.github.com>
2018-05-18 21:51:34 +1000

    *fixed bbugtest

--- src/hbmame/drivers/cinemat.cpp
+++ src/hbmame/drivers/cinemat.cpp
@@ -18,27 +18,19 @@ of the screen. Moving the mouse to the right will revert to changing colours aga
 
 #include "../mame/drivers/cinemat.cpp"
 
-class cinemat_hbmame : public cinemat_state
+class cinemat_hbmame : public cinemat_color_state
 {
 public:
 	cinemat_hbmame(const machine_config &mconfig, device_type type, const char *tag)
-		: cinemat_state(mconfig, type, tag)
+		: cinemat_color_state(mconfig, type, tag)
 		{ }
 
-	void bbugtest(machine_config &config);
 	void init_bbugtest();
 };
 
-MACHINE_CONFIG_START( cinemat_hbmame::bbugtest )
-	cinemat_jmi_32k(config);
-	MCFG_SCREEN_MODIFY("screen")
-	MCFG_SCREEN_VISIBLE_AREA(0, 1024, 0, 788)
-	MCFG_VIDEO_START_OVERRIDE(cinemat_state, cinemat_color)
-MACHINE_CONFIG_END
-
 void cinemat_hbmame::init_bbugtest()
 {
-	m_maincpu->space(AS_IO).install_read_handler(0x0c, 0x0f, read8_delegate(FUNC(cinemat_state::boxingb_dial_r),this));
+	m_maincpu->space(AS_IO).install_read_handler(0x0c, 0x0f, read8_delegate(FUNC(cinemat_hbmame::boxingb_dial_r),this));
 }
 
 
@@ -63,4 +55,4 @@ ROM_START( bbugtest )
 	ROM_LOAD("prom.e8",  0x180, 0x020, CRC(791ec9e1) SHA1(6f7fcce4aa3be9020595235568381588adaab88e) )
 ROM_END
 
-HACK(1981, bbugtest, 0, bbugtest, boxingb, cinemat_hbmame, bbugtest, ORIENTATION_FLIP_Y, "Test Rom", "Test Rom - Boxing Bugs", MACHINE_NO_SOUND_HW | MACHINE_SUPPORTS_SAVE )
+GAME(1981, bbugtest, 0, boxingb, boxingb, cinemat_hbmame, init_bbugtest, ORIENTATION_FLIP_Y, "Test Rom", "Test Rom - Boxing Bugs", MACHINE_NO_SOUND_HW | MACHINE_SUPPORTS_SAVE )
