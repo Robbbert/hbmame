b03a0ecacef5c0543364e83007210193a3b7ecce
Gaston90 <Gastonelias90@gmail.com>
2018-08-19 16:13:23 -0300

    Cleaning

--- src/hbmame/drivers/m92.cpp
+++ src/hbmame/drivers/m92.cpp
@@ -201,33 +201,6 @@ ROM_END
 
 ROM_START( hookjs01 )
 	ROM_REGION( 0x100000, "maincpu", 0 )
-	ROM_LOAD16_BYTE( "h-h0-g_ps01.3h", 0x000001, 0x040000, CRC(13fa1eca) SHA1(2a2a4d80a160535f667d6b425b532547018b0d22) )
-	ROM_LOAD16_BYTE( "h-l0-g_ps01.5h", 0x000000, 0x040000, CRC(f3efc301) SHA1(5664bf03054d7c23e90bffdc04d8ad55c1361ebf) )
-	ROM_LOAD16_BYTE( "h-h1.rom",  0x080001, 0x020000, CRC(264ba1f0) SHA1(49ecf9b3e5375629607fb747abe264406065580b) )
-	ROM_LOAD16_BYTE( "h-l1.rom",  0x080000, 0x020000, CRC(f9913731) SHA1(be7871d6843e76f66fae6b501c5ee83ccc366463) )
-
-	ROM_REGION( 0x20000, "soundcpu", 0 )
-	ROM_LOAD16_BYTE( "h-sh0-a.3l", 0x00001, 0x10000, CRC(bd3d1f61) SHA1(0c884a0b5519f9c0823128872baf7b0c4078e5c4) )
-	ROM_LOAD16_BYTE( "h-sl0-a.3n", 0x00000, 0x10000, CRC(76371def) SHA1(b7a86fd4eecdd8a538c32e08cd920c27bd50924b) )
-
-	ROM_REGION( 0x100000, "gfx1", 0 )
-	ROM_LOAD( "hook-c0.rom", 0x000000, 0x040000, CRC(dec63dcf) SHA1(e9869110f832d782c460b123928b042c65fdf8bd) )
-	ROM_LOAD( "hook-c1.rom", 0x040000, 0x040000, CRC(e4eb0b92) SHA1(159da3ec973490a153c69c96c1373cf4e0290736) )
-	ROM_LOAD( "hook-c2.rom", 0x080000, 0x040000, CRC(a52b320b) SHA1(1522562239bb3b93ef552c47445daa4ee021495c) )
-	ROM_LOAD( "hook-c3.rom", 0x0c0000, 0x040000, CRC(7ef67731) SHA1(af0b0ee6e1c06af04c609af7e077d4a7d76d8817) )
-
-	ROM_REGION( 0x400000, "gfx2", 0 )
-	ROM_LOAD( "hook-000.rom", 0x000000, 0x100000, CRC(ccceac30) SHA1(16e2b4393840344debe869034135feead7450184) )
-	ROM_LOAD( "hook-010.rom", 0x100000, 0x100000, CRC(8ac8da67) SHA1(a9b962cb0bc0d8bc3bda8a0ed1ce06641d666b41) )
-	ROM_LOAD( "hook-020.rom", 0x200000, 0x100000, CRC(8847af9a) SHA1(f82cdbd640fac373136219422172ca9fbf5d1830) )
-	ROM_LOAD( "hook-030.rom", 0x300000, 0x100000, CRC(239e877e) SHA1(445e1096619c4e3a2d5b50a645fd45bd7c501590) )
-
-	ROM_REGION( 0x80000, "irem", 0 )
-	ROM_LOAD( "hook-da.rom", 0x000000, 0x080000, CRC(88cd0212) SHA1(789532f5544b5d024d8af60eb8a5c133ae0d19d4) )
-ROM_END
-
-ROM_START( hookjs02 )
-	ROM_REGION( 0x100000, "maincpu", 0 )
 	ROM_LOAD16_BYTE( "h-h0-g_ps02.3h", 0x000001, 0x040000, CRC(5b836d2d) SHA1(5f0e34ea481a773f187565d6628c33447332dfdd) )
 	ROM_LOAD16_BYTE( "h-l0-g_ps02.5h", 0x000000, 0x040000, CRC(b3594f92) SHA1(b3becbbcf3f2774175ab8f4afacb71f533a2ff8d) )
 	ROM_LOAD16_BYTE( "h-h1.rom",  0x080001, 0x020000, CRC(264ba1f0) SHA1(49ecf9b3e5375629607fb747abe264406065580b) )
@@ -374,8 +347,7 @@ GAME( 200?, hooks04,     hook,        hook,          hook,      m92_state, empty
 GAME( 200?, hooks05,     hook,        hook,          hook,      m92_state, empty_init,    ROT0,   "RockyWall",         "Hook (Start Change)(World)", MACHINE_SUPPORTS_SAVE | MACHINE_NO_COCKTAIL)
 GAME( 200?, hookus01,    hook,        hook,          hook,      m92_state, empty_init,    ROT0,   "Unknown",         "Hook (plus)(US)", MACHINE_SUPPORTS_SAVE | MACHINE_NO_COCKTAIL)
 GAME( 200?, hookus02,    hook,        hook,          hook,      m92_state, empty_init,    ROT0,   "RockyWall",         "Hook (Start Change)(US)", MACHINE_SUPPORTS_SAVE | MACHINE_NO_COCKTAIL)
-GAME( 200?, hookjs01,    hook,        hook,          hook,      m92_state, empty_init,    ROT0,   "Unknown",         "Hook (1V4)(Japan)", MACHINE_SUPPORTS_SAVE | MACHINE_NO_COCKTAIL)
-GAME( 200?, hookjs02,    hook,        hook,          hook,      m92_state, empty_init,    ROT0,   "RockyWall",         "Hook (Start Change)(Japan)", MACHINE_SUPPORTS_SAVE | MACHINE_NO_COCKTAIL)
+GAME( 200?, hookjs01,    hook,        hook,          hook,      m92_state, empty_init,    ROT0,   "RockyWall",         "Hook (Start Change)(Japan)", MACHINE_SUPPORTS_SAVE | MACHINE_NO_COCKTAIL)
 // Ninja Baseball Bat Man
 GAME( 200?, nbbatmans01, nbbatman,    nbbatman,      nbbatman,  m92_state, init_bank,     ROT0,   "Unknown",         "Ninja Baseball Bat Man (X2 Edition)(World)", MACHINE_SUPPORTS_SAVE | MACHINE_NO_COCKTAIL )
 GAME( 200?, nbbatmans02, nbbatman,    nbbatman,      nbbatman,  m92_state, init_bank,     ROT0,   "Unknown",         "Ninja Baseball Bat Man (1V4)(World)", MACHINE_SUPPORTS_SAVE | MACHINE_NO_COCKTAIL )
