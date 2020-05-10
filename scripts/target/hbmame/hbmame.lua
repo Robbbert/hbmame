e3b0438a744501ab47c6a221a587fc63497bfce4
Robbbert <Robbbert@users.noreply.github.com>
2020-05-10 11:05:13 +1000

    *fixed build

--- scripts/target/hbmame/hbmame.lua
+++ scripts/target/hbmame/hbmame.lua
@@ -393,7 +393,6 @@ files {
 createHBMAMEProjects(_target, _subtarget, "capcom")
 files {
 	MAME_DIR .. "src/hbmame/drivers/sf.cpp",
-	MAME_DIR .. "src/mame/video/sf.cpp",
 	MAME_DIR .. "src/hbmame/drivers/1942.cpp",
 	MAME_DIR .. "src/mame/video/1942.cpp",
 	MAME_DIR .. "src/mame/audio/nl_1942.cpp",
