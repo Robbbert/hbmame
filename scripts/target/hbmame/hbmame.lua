9833a9036e388dd7f5514bc3750a1ba0d7dd3362
Robbbert <Robbbert@users.noreply.github.com>
2020-06-04 05:36:21 +1000

    *added zex8085

--- scripts/target/hbmame/hbmame.lua
+++ scripts/target/hbmame/hbmame.lua
@@ -1075,6 +1075,7 @@ files {
 	MAME_DIR .. "src/mame/video/timelimt.cpp",
 	MAME_DIR .. "src/hbmame/drivers/vamphalf.cpp",
 	MAME_DIR .. "src/hbmame/drivers/hh_sm510.cpp",
+	MAME_DIR .. "src/hbmame/drivers/zexall.cpp",
 }
 
 end
