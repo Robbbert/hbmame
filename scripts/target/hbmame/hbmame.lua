4154edead94dda2315764db73ca13142a3d665ee
Robbbert <Robbbert@users.noreply.github.com>
2022-08-07 01:46:20 +1000

    Begin work on CPS-2: ssf2

--- scripts/target/hbmame/hbmame.lua
+++ scripts/target/hbmame/hbmame.lua
@@ -866,7 +866,7 @@ files {
 	MAME_DIR .. "src/mame/video/sega16sp.cpp",
 	MAME_DIR .. "src/hbmame/drivers/segas16b.cpp",
 	MAME_DIR .. "src/mame/video/segas16b.cpp",
-	MAME_DIR .. "src/mame/drivers/segas18.cpp",
+	MAME_DIR .. "src/hbmame/drivers/segas18.cpp",
 	MAME_DIR .. "src/mame/video/segas18.cpp",
 	MAME_DIR .. "src/hbmame/drivers/segas32.cpp",
 	MAME_DIR .. "src/mame/machine/segas32.cpp",
