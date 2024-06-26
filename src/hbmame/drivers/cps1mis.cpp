936ef73fe08a31abf8cc75183c66ec394a3922fd
Robbbert <Robbbert@users.noreply.github.com>
2024-06-26 22:55:54 +1000

    Added sf2ce59.

--- src/hbmame/drivers/cps1mis.cpp
+++ src/hbmame/drivers/cps1mis.cpp
@@ -18455,6 +18455,38 @@ ROM_START( sf2ce58 ) // sf2hfsce - just says Street Fighter II' - nothing about 
 	ROM_LOAD( "sf2ce.key", 0x00, 0x80, CRC(35b37429) SHA1(b372cce106c0900554735c207fb333ac93554ec2) )
 ROM_END
 
+ROM_START( sf2ce59 )  // hack of sf2v004
+	ROM_REGION( CODE_SIZE, "maincpu", 0 )
+	ROM_LOAD16_WORD_SWAP( "c19s59.p1",    0x000000, 0x80000, CRC(9028b8b7) SHA1(be998b1b31bd3d72a970d50a2690551bfa0baeac) )
+	ROM_LOAD16_WORD_SWAP( "c19s59.p2",    0x080000, 0x80000, CRC(d275da79) SHA1(25b47c34e5dab0b6604f84059b6ad096c4528285) )
+	ROM_LOAD16_WORD_SWAP( "c19s59.p3",    0x100000, 0x80000, CRC(2f8a194f) SHA1(d11c3164d2ccafb350c50130707a87cd76b9dd4d) )
+
+	ROM_REGION( 0x600000, "gfx", 0 )
+	ROM_LOAD64_WORD( "c19.c01",  0x000000, 0x80000, CRC(03b0d852) SHA1(f370f25c96ad2b94f8c53d6b7139100285a25bef) )
+	ROM_LOAD64_WORD( "c19.c03",  0x000002, 0x80000, CRC(840289ec) SHA1(2fb42a242f60ba7e74009b5a90eb26e035ba1e82) )
+	ROM_LOAD64_WORD( "c19.c05",  0x000004, 0x80000, CRC(cdb5f027) SHA1(4c7d944fef200fdfcaf57758b901b5511188ed2e) )
+	ROM_LOAD64_WORD( "c19.c07",  0x000006, 0x80000, CRC(e2799472) SHA1(27d3796429338d82a8de246a0ea06dd487a87768) )
+	ROM_LOAD64_WORD( "c19.c09",  0x200000, 0x80000, CRC(ba8a2761) SHA1(4b696d66c51611e43522bed752654314e76d33b6) )
+	ROM_LOAD64_WORD( "c19.c11",  0x200002, 0x80000, CRC(e584bfb5) SHA1(ebdf1f5e2638eed3a65dda82b1ed9151a355f4c9) )
+	ROM_LOAD64_WORD( "c19.c13",  0x200004, 0x80000, CRC(21e3f87d) SHA1(4a4961bb68c3a1ce15f9d393d9c03ecb2466cc29) )
+	ROM_LOAD64_WORD( "c19.c15",  0x200006, 0x80000, CRC(befc47df) SHA1(520390420da3a0271ba90b0a933e65143265e5cf) )
+	ROM_LOAD64_WORD( "c19.c17",  0x400000, 0x80000, CRC(960687d5) SHA1(2868c31121b1c7564e9767b9a19cdbf655c7ed1d) )
+	ROM_LOAD64_WORD( "c19.c19",  0x400002, 0x80000, CRC(978ecd18) SHA1(648a59706b93c84b4206a968ecbdc3e834c476f6) )
+	ROM_LOAD64_WORD( "c19.c21",  0x400004, 0x80000, CRC(d6ec9a0a) SHA1(ed6143f8737013b6ef1684e37c05e037e7a80dae) )
+	ROM_LOAD64_WORD( "c19.c23",  0x400006, 0x80000, CRC(ed2c67f6) SHA1(0083c0ffaf6fe7659ff0cf822be4346cd6e61329) )
+
+	ROM_REGION( 0x18000, "audiocpu", 0 )
+	ROM_LOAD( "c19.m1",    0x00000, 0x08000, CRC(08f6b60e) SHA1(8258fcaca4ac419312531eec67079b97f471179c) )
+	ROM_CONTINUE(              0x10000, 0x08000 )
+
+	ROM_REGION( 0x40000, "oki", 0 )
+	ROM_LOAD( "c14.v1",    0x00000, 0x20000, CRC(7f162009) SHA1(346bf42992b4c36c593e21901e22c87ae4a7d86d) )
+	ROM_LOAD( "c14.v2",    0x20000, 0x20000, CRC(beade53f) SHA1(277c397dc12752719ec6b47d2224750bd1c07f79) )
+
+	ROM_REGION( 0x80, "control", 0 )
+	ROM_LOAD( "sf2ce.key", 0x00, 0x80, CRC(35b37429) SHA1(b372cce106c0900554735c207fb333ac93554ec2) )
+ROM_END
+
 ROM_START( sf2koryu01 )
 	ROM_REGION( CODE_SIZE, "maincpu", 0 )
 	ROM_LOAD16_BYTE( "c19koryus01.p1",   0x000000, 0x80000, CRC(075b2f19) SHA1(65e3c21d328c0039a5537c23681796de196cab83) )
@@ -28544,6 +28576,7 @@ GAME( 1992, sf2ce55,       sf2ce,    cps1_12MHz, sf2hack,  cps_state, init_sf2ha
 GAME( 2018, sf2ce56,       sf2ce,    cps1_12MHz, sf2,      cps_state, init_cps1,     ROT0, "Drakon", "Street Fighter II': Champion Edition (Punishment Edition Simplified, 2018-06-26)", MACHINE_SUPPORTS_SAVE )
 GAME( 2015, sf2ce57,       sf2ce,    cps1_12MHz, sf2hack,  cps_state, init_sf2hack,  ROT0, "hack", "Street Fighter II': Champion Edition (Hacks Unknown, 2015-10-12)", MACHINE_SUPPORTS_SAVE )
 GAME( 2024, sf2ce58,       sf2hf,    cps1_12MHz, sf2,      cps_state, init_cps1,     ROT0, "Brazilian Mr Street Fighter", "Street Fighter II' (SrSreet edition)", MACHINE_SUPPORTS_SAVE ) // bad tile for Blanka on player select screen
+GAME( 2020, sf2ce59,       sf2ce,    cps1_12MHz, sf2,      cps_state, init_cps1,     ROT0, "Mameplus", "Street Fighter II' (Golden Magic)", MACHINE_SUPPORTS_SAVE ) // bad tile for Blanka on player select screen
 GAME( 1992, sf2koryu01,    sf2ce,    cps1_12MHz, sf2hack,  cps_state, init_sf2hack,  ROT0, "King Of Destruction", "Street Fighter II': Champion Edition (Huaping Repair)", MACHINE_SUPPORTS_SAVE )
 GAME( 1992, sf2koryu02,    sf2ce,    cps1_12MHz, sf2hack,  cps_state, init_sf2hack,  ROT0, "bootleg", "Street Fighter II': Champion Edition: Xiang Long (Chinese bootleg set 3)", MACHINE_SUPPORTS_SAVE )
 GAME( 1992, sf2koryu03,    sf2ce,    cps1_12MHz, sf2hack,  cps_state, init_sf2hack,  ROT0, "bootleg", "Street Fighter II': Champion Edition: Xiang Long (Chinese bootleg set 4)", MACHINE_SUPPORTS_SAVE )
