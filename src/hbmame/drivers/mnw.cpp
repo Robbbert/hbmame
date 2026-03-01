3f53725de2963d48b3978c6b4a0148490abadd37
Robbbert <Robbbert@users.noreply.github.com>
2026-03-01 15:41:20 +1100

    Added kof98s53,54,56,57,58,59

--- src/hbmame/drivers/mnw.cpp
+++ src/hbmame/drivers/mnw.cpp
@@ -23997,6 +23997,32 @@ ROM_START( kof98s26 ) // Anniversary Edition : all confirmed - crash in attract 
 	ROM_LOAD16_BYTE( "242ae.c12", 0x5000001, 0x800000, CRC(f165589e) SHA1(6bdcb1dd9ce507fcae40d0aba42a2c1300987479) )
 ROM_END
 
+ROM_START( kof98s55 ) //kof98cb - address error at start
+	ROM_REGION( 0x500000, "maincpu", 0 )
+	ROM_LOAD16_WORD_SWAP( "242_hc60.p1", 0x000000, 0x100000, CRC(2565e431) SHA1(cc264ad6c969f67688ca3187425760ba70932243) )
+	ROM_LOAD16_WORD_SWAP( "242_hc60.p2", 0x100000, 0x400000, CRC(d34a4d38) SHA1(7af1d73a6b82f7be847719b013f77a5692bd0d49) )
+
+	NEO_SFIX_128K( "242_hc19.s1", CRC(7333d8b0) SHA1(43d441133d572f65e9e19839ae2557ad86e393a1) )
+
+	NEO_BIOS_AUDIO_256K( "242_hc19.m1", CRC(db046fc4) SHA1(4b793a125bade067ef0fdcd1775d135c646c6b94) )
+
+	ROM_REGION( 0x1000000, "ymsnd:adpcma", 0 )
+	ROM_LOAD( "242.v1", 0x000000, 0x400000, CRC(b9ea8051) SHA1(49606f64eb249263b3341b4f50cc1763c390b2af) )
+	ROM_LOAD( "242.v2", 0x400000, 0x400000, CRC(cc11106e) SHA1(d3108bc05c9bf041d4236b2fa0c66b013aa8db1b) )
+	ROM_LOAD( "242.v3", 0x800000, 0x400000, CRC(044ea4e1) SHA1(062a2f2e52098d73bc31c9ad66f5db8080395ce8) )
+	ROM_LOAD( "242.v4", 0xc00000, 0x400000, CRC(7985ea30) SHA1(54ed5f0324de6164ea81943ebccb3e8d298368ec) )
+
+	ROM_REGION( 0x4000000, "sprites", 0 )
+	ROM_LOAD16_BYTE( "242_hc19.c1",  0x0000000, 0x800000, CRC(066db0a6) SHA1(ea200b79bf60d2d9a025da873173cc4704d254b0) )
+	ROM_LOAD16_BYTE( "242_hc19.c2",  0x0000001, 0x800000, CRC(99d0b0fa) SHA1(b56813917de0c19858c13e51888b6af2030bc324) )
+	ROM_LOAD16_BYTE( "242_hc19.c3",  0x1000000, 0x800000, CRC(ea84bdae) SHA1(27108247ed7406060f0b149a9dfa64869a6bbcf2) )
+	ROM_LOAD16_BYTE( "242_hc19.c4",  0x1000001, 0x800000, CRC(2c17ac8e) SHA1(fc704f5cf358df81a88d20932be3edffe202dea9) )
+	ROM_LOAD16_BYTE( "242.c5", 0x2000000, 0x800000, CRC(9d10bed3) SHA1(4d44addc7c808649bfb03ec45fb9529da413adff) )
+	ROM_LOAD16_BYTE( "242.c6", 0x2000001, 0x800000, CRC(da07b6a2) SHA1(9c3f0da7cde1ffa8feca89efc88f07096e502acf) )
+	ROM_LOAD16_BYTE( "242.c7", 0x3000000, 0x800000, CRC(f6d7a38a) SHA1(dd295d974dd4a7e5cb26a3ef3febcd03f28d522b) )
+	ROM_LOAD16_BYTE( "242.c8", 0x3000001, 0x800000, CRC(c823e045) SHA1(886fbf64bcb58bc4eabb1fc9262f6ac9901a0f28) )
+ROM_END
+
 
 
 GAME( 2009, kof98ae,   kof98,    neogeo_noslot, neogeo, neogeo_state, init_neogeo,   ROT0, "hack", "Kof'98 (Anniversary Edition)", MACHINE_NOT_WORKING | MACHINE_SUPPORTS_SAVE )
@@ -24100,6 +24126,7 @@ GAME( 2016, kof98s21,       kof98,    neogeo_noslot, neogeo, neogeo_state,      
 GAME( 1998, kof98s23,       kof98,    neogeo_noslot, neogeo, neogeo_state,         init_neogeo,    ROT0, "hack",    "Kof'98 (Combo, 2017-07-27)", MACHINE_SUPPORTS_SAVE )
 GAME( 1998, kof98s25,       kof98,    neogeo_noslot, neogeo, neogeo_state,         init_neogeo,    ROT0, "Hassan & Imran Butt", "Kof'98 (Anniversary Edition Unknown Date Rev.1)", MACHINE_SUPPORTS_SAVE )
 GAME( 1998, kof98s26,       kof98,    neogeo_noslot, neogeo, neogeo_state,         init_neogeo,    ROT0, "hack",    "Kof'98 (Anniversary Edition Unknown Date Rev.2)", MACHINE_SUPPORTS_SAVE )
+GAME( 2020, kof98s55,       kof98,    neogeo_noslot, neogeo, neogeo_state,    init_neogeo,   ROT0, "Ivex",  "Kof'98 (Combo, 2018-05-05)", MACHINE_SUPPORTS_SAVE )
 
 
 ROM_START( kof99ae0 ) /* Original Version - Decrypted GFX */
@@ -25784,6 +25811,24 @@ ROM_END
 
 GAME( 2025, violentv,     neogeo,   neogeo_noslot,   neogeo,  neogeo_state, init_neogeo,   ROT0, "Balek Corp", "Violent Vengeance: the universe hero (beta 1)", MACHINE_SUPPORTS_SAVE )
 GAME( 2026, violentv,     neogeo,   neogeo_noslot,   neogeo,  neogeo_state, init_neogeo,   ROT0, "Balek Corp", "Violent Vengeance: the universe hero (2026-01-10)", MACHINE_SUPPORTS_SAVE )
+
+// 517: Grime: doesn't work
+ROM_START( grime )
+	ROM_REGION( 0x100000, "maincpu", 0 )
+	ROM_LOAD16_WORD_SWAP( "517.p1", 0x000000, 0x080000, CRC(e42ed460) SHA1(6e9e413fd67bb8b07ff2233adaa736f8f0e7510e) )
+
+	NEO_SFIX_128K( "517.s1", CRC(9ecf7b85) SHA1(0f02b25f3ed583c1d63532a4222db1f9877b8b04) )
+
+	// m1 has been expanded from 0xf803 bytes to 0x10000 bytes, to fit in with our macro
+	NEO_BIOS_AUDIO_64K( "517.m1", CRC(6eb2bcc0) SHA1(2d0d036e7d744c8738ff57ab9d3c75d9398f2cac) )
+
+	ROM_REGION( 0x400000, "sprites", 0 )
+	ROM_LOAD16_BYTE( "499.c1", 0x200000, 0x100000, CRC(479543cf) SHA1(772690c872632320133a799aa41f6e68a8d07a4c) )
+	ROM_LOAD16_BYTE( "499.c2", 0x200001, 0x100000, CRC(1f6431d5) SHA1(7c90d6ec9df9e6223a066c338b7a7886071370cf) )
+ROM_END
+
+GAME( 2019, grime,        neogeo,   neogeo_noslot,   neogeo,  neogeo_state, init_neogeo,   ROT0, "ChibiAkumas", "Grime", MACHINE_SUPPORTS_SAVE )
+
 
 //-------------------------------------------------------------------------------------------------------------------
 
