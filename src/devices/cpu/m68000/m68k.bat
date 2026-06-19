ff2b2eeb1a768b5d19842739a6740d36949ee4dc
Robbbert <Robbbert@users.noreply.github.com>
2026-06-20 01:52:02 +1000

    z

--- src/devices/cpu/m68000/m68k.bat
+++
@@ -1,1 +0,0 @@
-m68kmake.py m68k_in.lst m68kops.h m68kops.cpp
--- src/tools/old/minimaws.bat
+++
@@ -1,6 +0,0 @@
-cd scripts\minimaws
-del minimaws.sqlite3
-python minimaws.py load --executable ..\..\hbmame.exe
-python minimaws.py serve
-cd..
-cd..
